8522d8c231ed7b2d2fccc96a8be3f8a4ff95c40e
MarwaneBella <bellaworks00@gmail.com>
2021-12-27 01:51:46 +0100

    marwane

--- app/src/main/java/com/example/services/Login.java
+++ app/src/main/java/com/example/services/Login.java
@@ -1,35 +1,100 @@
 package com.example.services;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class Login extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Login extends AppCompatActivity implements View.OnClickListener{
+    private FirebaseAuth mAuth;
+    private EditText etEmail, etPassword;
+    private Button btnLog;
+    private TextView tvReg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        final EditText etEmail = (EditText) findViewById(R.id.TextEmail);
-        final EditText etPassword = (EditText) findViewById(R.id.TextPassword);
-        final Button bLogin = (Button) findViewById(R.id.btnLog);
-        final TextView tvReg = (TextView) findViewById(R.id.TextReg);
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        btnLog = (Button) findViewById(R.id.btnLog);
+        btnLog.setOnClickListener(this);
+        tvReg = (TextView) findViewById(R.id.tvReg);
+        tvReg.setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();
 
-        tvReg.setOnClickListener(v -> {
-            Intent intent = new Intent(Login.this, Register.class);
-            Login.this.startActivity(intent);
-        });
 
-        bLogin.setOnClickListener(v -> {
-            final String email = etEmail.getText().toString();
-            final String password = etPassword.getText().toString();
-            Intent intentt = new Intent(Login.this, MainActivity.class);
-            Login.this.startActivity(intentt);
-
-        });
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.tvReg:
+                startActivity(new Intent(Login.this,Register.class));
+                break;
+            case R.id.btnLog:
+                loginUser();
+                break;
+        }
+    }
+
+    private void loginUser() {
+
+        String email = etEmail.getText().toString().trim();
+        String password = etPassword.getText().toString().trim();
+
+
+        if(email.isEmpty()){
+            etEmail.setError("Email is required!");
+            etEmail.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            etEmail.setError("Please provide valid email!");
+            etEmail.requestFocus();
+            return;
+        }
+        if(password.isEmpty()){
+            etPassword.setError("Password is required!");
+            etPassword.requestFocus();
+            return;
+        }
+        if(password.length() < 6){
+            etPassword.setError("Min password length should be 6 characters!");
+            etPassword.requestFocus();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(Login.this,"Successfully!",Toast.LENGTH_LONG).show();
+                            startActivity(new Intent(Login.this,MainActivity.class));
+
+                        }
+                        else {
+                            Toast.makeText(Login.this,"Failed!",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+    }
+
 }
